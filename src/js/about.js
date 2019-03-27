cf45326a247ec54ccaf4f30379461fc288e8c307
Shrihari <shrihari.murali@galepartners.com.com>
2019-03-27 13:55:50 +0530

    Initial commit

---
+++ src/js/about.js
@@ -0,0 +1,2 @@
+require('normalize.css/normalize.css');
+alert('About page is loaded');
---
+++ src/js/home.js
@@ -0,0 +1,3 @@
+require('normalize.css/normalize.css');
+require('../sass/product.scss');
+alert('Home page is loaded');
